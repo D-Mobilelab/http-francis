7c32ce62493cf79724ed3a988cc00ab0c8a376c4
pasquale <pasqualemangialavori@buongiorno.com>
2016-07-13 12:06:06 +0200

    Version 0.1.3

---
+++ 0.1.3/js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "version",
+      "id": "Francis",
+      "shortName": "Francis",
+      "type": "object",
+      "moduleName": "Francis",
+      "shortDescription": "an http module to make requests over the net with retry and interval between them",
+      "keywords": "francis function http interval module net object requests retry version"
+    },
+    {
+      "section": "version",
+      "id": "Francis:Http",
+      "shortName": "Http",
+      "type": "object",
+      "moduleName": "Francis:Http",
+      "shortDescription": "an http class to make requests over the net with retry and interval between them",
+      "keywords": "appendchild async attempt attempts blob body callback case class content-length createelement default delete document example fails francis gettask header headers http http-francis https image img imgtag interval js jsonp method mimetype ms nan net object onend onprogress options percentage png post promise reason request requestparams requests require response responsetype retry retryafter src synchrounous timeout type url values var version"
+    }
+  ],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/main",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
